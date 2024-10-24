7f0e7cead849ff22800082c076ba6ca921a65501
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 12:17:45 -0300

    first commit

---
+++ src/main/java/com/consulting/boleto_service/BoletoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.boleto_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletoServiceApplication.class, args);
+	}
+
+}
